68ad85b94631ad3891fabb09b65f4bb767bbb067
Faustina Maria <faustina.giaquinta@gmail.com>
2020-10-08 16:10:18 -0400

    added create_spend_chart function to plot expenses

--- budget.py
+++ budget.py
@@ -85,4 +85,33 @@ class Category:
 
 
 def create_spend_chart(categories):
-    pass
+    cat_ledgers = [cat.ledger for cat in categories]
+    withdrawals = [-sum([exp['amount'] for exp in ledger_ if exp['amount'] < 0])
+                   for ledger_ in cat_ledgers]
+    total_withdrawals = sum(withdrawals)
+    withdrawals_idx = [
+        10 - int(i * 100 // total_withdrawals) // 10 for i in withdrawals]
+
+    num_cat = len(categories)
+    inner_space = num_cat - 1
+    cats_str = num_cat * '{}  '
+    cat_names = [cat.name for cat in categories]
+    cat_names = [cat.ljust(len(max(cat_names, key=len)), ' ')
+                 for cat in cat_names]
+
+    percentages = list(range(100, -10, -10))
+    chart_str = ['Percentage spent by category']
+
+    for idx, i in enumerate(percentages):
+        cat_percentages = ' ' + cats_str.format(
+            *['o' if wi <= idx else ' ' for wi in withdrawals_idx])
+        chart_str.append(f"{str(i).rjust(3, ' ')}|" + cat_percentages)
+        if not i:
+            chart_str.append(' ' * 4 + f"-{'-'*num_cat}{'--'*inner_space}--")
+            for i in range(len(max(cat_names, key=len))):
+                chart_str.append(' ' * 5 + cats_str.format(*
+                                                           [cat[i] for cat in cat_names]))
+
+    chart_str = '\n'.join(chart_str)
+
+    return chart_str
